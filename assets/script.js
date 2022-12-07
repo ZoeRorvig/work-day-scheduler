52e9af0e8bc17c2590fcc128000c381d33345203
Zoe Rorvig <zrorvig@gmail.com>
2022-12-06 22:05:34 -0500

    added code to pull current time

--- assets/script.js
+++ assets/script.js
@@ -24,6 +24,10 @@ $(window).on("load", function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
+  var currentTime = dayjs(new Date()).format("H");
+  console.log(currentTime);
+
+
   // Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
@@ -40,7 +44,7 @@ $(window).on("load", function () {
 
   // Code to display the current date in the header of the page.
   var currentDate = $("#currentDay");
-  var date = dayjs(new Date()).format("dddd, MMMM Do");
-    currentDate.text(date);
+  var date = dayjs(new Date()).format("dddd, MMMM D");
+      currentDate.text(date);
 
 });
